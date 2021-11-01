08df2d601809333cb8da78479b8c69179fc67fce
Abbos Sharofov <abbossharofov1996@gmail.com>
2021-11-01 07:36:17 +0500

    Create Interview-Eng.py

---
+++ Interview-Eng.py
@@ -0,0 +1,10 @@
+#interview
+ism=input("Kompyuter: -Salam Aleikum.\nWhat is your name? \n\nSiz:- ")
+print("\nKompyuter: -Nice to meet you "+ism)
+yosh=input("Kompyuter: -How old are you?\nSiz:-(With number)")
+yosh=int(yosh)
+tyil=2021-yosh
+tyil=str(tyil)
+print("Then "+"-You was born in "+tyil+". Excellent!")
+tkunoy=input("Kompyuter: -Qaysi kunda tug'ilgansiz?\n\n")
+print("Thank you!")
